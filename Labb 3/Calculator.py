85365aeaa920e6d659cb58710e31cb9aa35d9b61
Fabian Forsman <fabian.forsman@gmail.com>
2021-10-01 16:20:08 +0200

    Co-authored-by: Krysset <maxebrax@gmail.com>

--- Labb 3/Calculator.py
+++ Labb 3/Calculator.py
@@ -27,35 +27,28 @@ MISSING_OPERATOR: str = "Missing operator or parenthesis"
 OP_NOT_FOUND:     str = "Operator not found"
 OPERATORS:        str = "+-*/^"
 ### TEST NUMBERS ###
-# (5+4)*(7-3)
-# (5*(3-1)+2)
+# (5+4)*(7-3) = 36
+# (5*(3-1)+2) = 
 # (5+10)*64^8+(8 + (4*(3+4))
-# 2*((5-1)*(2+2))
 
+
+# 2*((5-1)*(2+2)) = 16
+# ((5-1)-(3-(2+3)))
+# 5 1 - 2 2 + * 2 *
 def infix_to_postfix(tokens):
     tokens = "".join(tokens)  # To support both GUI and REPL
+    test()
     ### Add in this order to stack
     stack = deque()
     # First check for parenthesis
-    
-    if find_all(tokens, "(") != find_all(tokens, ")"):
-        # TODO: error
+    if len(find_all(tokens, "(")) > 0 or len(find_all(tokens, ")")) > 0:
+        paren_depth(tokens)
+        # handle_parenthesis()
+    else:
         pass
     positions = find_all(tokens, "(")
-    print(f"Input: {tokens}")
-    for i in range(len(positions), 0, -1):
-        
-        first_parenthesis = positions[i-1]
-        second_parenthesis = tokens.rfind(")") + 1
-        substring = tokens[first_parenthesis: second_parenthesis]
-        tokens = tokens.replace(substring, "")
-        print(f"Substr: {substring}")
-        print(f"Input removed substr: {tokens}")
-
-    # while subtoken.find("(") != -1:
-    #     if tokens.rfind(")") == -1:
-    #         return ValueError(MISSING_OPERATOR)
-    #     subtoken = tokens[tokens.find("("): tokens.rfind(")")]
+    print(f"Positions of opening parenthesis: {positions}")
+    print(f"Input was: {tokens}")
     
     # Second check power
     # Third check multiplication and division
@@ -64,6 +57,53 @@ def infix_to_postfix(tokens):
 
 
 # Rekursiv metod som appendar till en stack för paranteser?
+def handle_parenthesis():
+    if not find_all(tokens, "(") == find_all(tokens, ")"):
+        return ValueError(MISSING_OPERATOR)
+    parentheses = {"open": "(", "closed": ")"}
+
+
+def paren_depth(tokens) -> list:
+    print(tokens)
+    depth: int = -1
+    parentheses = {"open": "(", "closed": ")"}
+    paren_pos_pairs = []
+    open: int = 0
+    close: int = 0
+    for i in range(len(tokens)):
+        if tokens[i] in parentheses["open"]:
+            print(f'Token "{tokens[i]}" at {i} was open')
+            open = i
+            depth += 1
+        elif tokens[i] in parentheses["closed"]:
+            print(f'Token "{tokens[i]}" at {i} was closed')
+            close = i
+            while len(paren_pos_pairs) <= depth:
+                paren_pos_pairs.append([])
+            paren_pos_pairs[depth].append((open, close))
+            depth -= 1
+    print(paren_pos_pairs)
+
+def paren_depth_v2(tokens: str) -> list:
+    depth = 0
+    #handles parenthesis splits
+    occurence = tokens.find("(")
+    while len(find_all(tokens, "(")) > 0:
+        for i in range(len(tokens)):
+            if tokens[i] == "(":
+                depth += 1
+            elif tokens[i] == ")":
+                depth -= 1
+            if depth == 0 and tokens[i] == ")":
+                # occurence + 1 only takes the arguments inside the parentheses
+                # print(f"Split: {tokens[occurence + 1 : i]}")
+                paren_depth_v2(tokens[occurence + 1: i])
+                tokens = tokens.replace(tokens[occurence: i + 1], "")  # Removes parantheses from tokens
+                break
+        occurence = tokens.find("(")
+    print(f"Result after split: {tokens}")
+    # Handle adding to stack
+    
 
 
 def find_all(a_str, sub):
@@ -132,3 +172,16 @@ def tokenize(expr: str):
     return None   # TODO
 
 # TODO Possibly more methods
+
+def test():
+    tokens = ["2*((5-1)*(2+2))", "(5*(3-1)+2)", "(2+(3-1)*5)"]
+    assert len(find_all(tokens[0], "(")) == 3
+    assert find_all(tokens[0], "(") == [2, 3, 9]
+    # paren_depth(tokens)
+    # assert paren_depth(tokens) == [[(2, 14)], [(3, 7), (9, 13)]]
+
+    for i in range(len(tokens)):
+        print(tokens[i])
+        paren_depth_v2(tokens[i])
+
+    print("Testing Completed")
