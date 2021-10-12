a5370ec7514491d73aab7c82d66af900fc343dcf
Supergamer1337 <felix.b.aronsson@gmail.com>
2021-10-12 13:12:57 +0200

    V2 ready
    Co-authored-by: Krysset <maxebrax@gmail.com>
    Co-authored-by: Fabian Forsman <FabianForsman@users.noreply.github.com>

--- Labb 3/Version 2/Calculator.py
+++ Labb 3/Version 2/Calculator.py
@@ -25,7 +25,12 @@ MISSING_OPERAND:  str = "Missing or bad operand"
 DIV_BY_ZERO:      str = "Division with 0"
 MISSING_OPERATOR: str = "Missing operator or parenthesis"
 OP_NOT_FOUND:     str = "Operator not found"
+NEGATIVE_NUMBER:  str = "Negative number found"  
+INCORRECT_COMMA:  str = "Comma found in incorrect location"
+ILLEGAL_CHAR:     str = "Illegal char found"
+
 OPERATORS:        str = "+-*/^"
+VALID_CHARS:      str = "()." + OPERATORS
 ### TEST NUMBERS ###
 # (5+4)*(7-3) = 36
 # (5*(3-1)+2) = 
@@ -35,97 +40,29 @@ OPERATORS:        str = "+-*/^"
 # 2*((5-1)*(2+2)) = 32
 # ((5-1)-(3-(2+3)))
 # 5 1 - 2 2 + * 2 *
-def infix_to_postfix(tokens):
-    tokens = "".join(tokens)  # To support both GUI and REPL
-    test()
-    ### Add in this order to stack
-    stack = deque()
-    valid_parentheses(tokens)
-    print(f"Input was: {tokens}")
-    token_to_stack(tokens, stack)
-    # Second check power
-    # Third check multiplication and division
-    # Fourth check addition and subtraction
-    return stack
-
-
-def token_to_stack(tokens: str, stack: deque) -> deque:
-    tokens = split_parenthesis(tokens, stack)
-    # print(f"Result after split: {tokens}")
-    fill_stack(tokens, stack)
-    print(stack)
-    return stack
-
-
-def split_parenthesis(tokens, stack):
-    depth = 0
-    # Breaks down parentheses
-    occurrence = tokens.find("(")
-    while len(find_all(tokens, "(")) > 0:
-        for i in range(len(tokens)):
-            if tokens[i] == "(":
-                depth += 1
-            elif tokens[i] == ")":
-                depth -= 1
-            if depth == 0 and tokens[i] == ")":
-                # occurrence + 1 only takes the arguments inside the parentheses
-                # print(f"Split: {tokens[occurrence + 1: i]}")  # Uncomment for more debug info
-                token_to_stack(tokens[occurrence + 1: i], stack)
-                tokens = tokens.replace(tokens[occurrence: i + 1], "")  # Removes parentheses from tokens
-                break
-        occurrence = tokens.find("(")
-    return tokens
-
-
-def fill_stack(tokens, stack):
-    for precedence in range(2, -1, -1):
-        # print(f"Current precedence: {precedence}")
-        operator_positions = get_operator_positions(tokens, precedence)
-        while len(operator_positions) != 0:
-            operator = tokens[operator_positions[0]]
-            # print(f"Current op: {operator}")
-            left, right = get_adjacent(tokens, operator)
-            if left is not None:
-                stack.append(left)
-            else:
-                left = ""
-            if right is not None:
-                stack.append(right)
-            else:
-                right = ""
-            stack.append(operator)
-            string = f"{left}{operator}{right}"
-            # print(string)
-            tokens = tokens.replace(string, "")
-            operator_positions = get_operator_positions(tokens, precedence)
-
-
-def get_operator_positions(tokens, precedence):
-    operator_positions: list[int] = []
-    for pos, char in enumerate(tokens):
-        op_precedence = get_precedence(char)
-        if op_precedence == precedence:
-            operator_positions.append(pos)
-    return operator_positions
-
-
-def get_adjacent(tokens: str, operator: str):
-    right = convert_int(tokens, operator, 1)
-    left = convert_int(tokens, operator, -1)
-    return left, right
-
-
-def convert_int(tokens: str, operator: str, offset: int):
-    try:
-        pos = tokens.index(operator) + offset
-        if -1 < pos < len(tokens):
-            num = int(tokens[pos])
+def infix_to_postfix(tokens) -> list:
+    tokens: list[str] = tokenize(tokens)
+    temp = []
+    postfix_expr = []
+    for index, elem in enumerate(tokens):
+        if elem[0].isdigit():
+            postfix_expr.append(elem)
+        elif elem == "(":
+            temp.append(elem)
+        elif elem == ")":
+            while len(temp) != 0 and not temp[-1] == "(":
+                postfix_expr.append(temp.pop())
+            temp.pop()
         else:
-            return None
-    except ValueError:
-        return None
-    return num
-    
+            if len(temp) != 0 and get_precedence(elem) > get_precedence(temp[-1]):
+                temp.append(elem)
+            else:
+                while len(temp) != 0 and get_precedence(elem) <= get_precedence(temp[-1]):
+                    postfix_expr.append(temp.pop())
+                temp.append(elem)
+    while len(temp) != 0:
+        postfix_expr.append(temp.pop())
+    return postfix_expr
 
 def find_all(a_str, sub):
     start = 0
@@ -139,17 +76,37 @@ def find_all(a_str, sub):
 
 
 # -----  Evaluate RPN expression -------------------
-def eval_postfix(postfix_tokens):
-    return "DAB"  # TODO
+def eval_postfix(postfix: list) -> str:
+    index = 0
+    while not len(postfix) == 1:
+        if isinstance(postfix[index], str) and postfix[index] in OPERATORS:
+            variables = []
+            for i in range(2):
+                variables.append(float(postfix.pop(index-2)))
+            variables.append(postfix.pop(index-2))
+            variables.reverse()
+            result = apply_operator(*variables)
+            if result is nan:
+                raise ValueError(DIV_BY_ZERO)
+            postfix.insert(index-2, result)
+            index = 0
+        else:
+            index += 1
+    result = postfix[0]
+    print(result)
+    return str(result)
 
 
 # Method used in REPL
 def eval_expr(expr: str):
-    if len(expr) == 0:
-        return nan
-    tokens = expr.split()
-    postfix_tokens = infix_to_postfix(tokens)
-    return eval_postfix(postfix_tokens)
+    try:
+        if len(expr) == 0:
+            return nan
+        tokens = expr.split()
+        postfix_tokens = infix_to_postfix(tokens)
+        return eval_postfix(postfix_tokens)
+    except ValueError as err:
+        return f"ValueError: {err.args[0]}"
 
 
 def apply_operator(op: str, d1: float, d2: float):
@@ -165,14 +122,13 @@ def apply_operator(op: str, d1: float, d2: float):
 
 def get_precedence(op: str):
     op_switcher = {
-        "+": 0,
-        "-": 0,
-        "*": 1,
-        "/": 1,
-        "^": 2
+        "+": 1,
+        "-": 1,
+        "*": 2,
+        "/": 2,
+        "^": 3
     }
     return op_switcher.get(op, -1)
-
 
 class Assoc(Enum):
     LEFT = 1
@@ -189,10 +145,83 @@ def get_associativity(op: str):
 
 
 # ---------- Tokenize -----------------------
-def tokenize(expr: str):
-    return None   # TODO
+def tokenize(expr: str) -> list[str]:
+    expr = "".join(expr)  # To support both GUI and REPL
+    expr = comma_to_dot(expr)
+    error_check_string(expr)
+    return string_to_list(expr)
 
-# TODO Possibly more methods
+
+def error_check_string(tokens):
+    valid_parentheses(tokens)
+    contains_illegal_char(tokens)
+
+
+def valid_parentheses(tokens):
+    if not len(find_all(tokens, "(")) == len(find_all(tokens, ")")):
+        raise ValueError(MISSING_OPERATOR)
+
+
+def contains_illegal_char(tokens):
+    if tokens[0] == "-":
+        raise ValueError(MISSING_OPERAND)
+    i = 0
+    while i < len(tokens):
+        # Checks that require current char
+        if not tokens[i].isdigit() and not tokens[i] in VALID_CHARS:
+            raise ValueError(ILLEGAL_CHAR)
+        # Checks that require last char
+        if i > 0:
+            # Negative number
+            if negative_number(tokens, i):
+                raise ValueError(NEGATIVE_NUMBER)
+            # Double comma
+            if invalid_before_comma(tokens, i):
+                raise ValueError(INCORRECT_COMMA)
+        # Checks that require last and next char
+        if trailing_operator(tokens, i):
+            raise ValueError(MISSING_OPERAND)
+        i += 1
+
+
+def negative_number(tokens: str, pos: int) -> bool:
+    current_sign = tokens[pos] == "-"
+    invalid_neighbour = (tokens[pos - 1] in OPERATORS or tokens[pos - 1] == "(")
+    return current_sign and invalid_neighbour
+
+
+def invalid_before_comma(tokens: str, pos: int) -> bool:
+    is_comma = tokens[pos] == '.'
+    invalid_neighbour = (tokens[pos - 1] == '.' or not tokens[pos - 1].isdigit())
+    return is_comma and invalid_neighbour
+
+
+def trailing_operator(tokens: str, pos: int) -> bool:
+    is_operator = tokens[pos] in OPERATORS
+    out_of_string = pos + 1 == len(tokens) or pos  == 0
+    valid_neighbour = tokens[pos - 1].isdigit() or tokens[pos - 1] in ")"
+    return is_operator and (out_of_string or not valid_neighbour)
+
+
+def string_to_list(tokens: str):
+    start = end = 0
+    result = []
+    while end < len(tokens):
+        if not tokens[start].isdigit():
+            result.append(tokens[end])
+            start = end = start + 1
+        else:
+            end += 1
+            if end == len(tokens) or not tokens[end].isdigit() and not tokens[end] == ".":
+                result.append(tokens[start:end])
+                if end < len(tokens):
+                    result.append(tokens[end])
+                end = start = end + 1
+    return result
+
+
+def comma_to_dot(tokens: str) -> str:
+    return tokens.replace(",", ".")
 
 
 def valid_parentheses(tokens):
@@ -200,20 +229,73 @@ def valid_parentheses(tokens):
         return ValueError(MISSING_OPERATOR)
 
 
+def get_operator_positions(tokens, precedence):
+    operator_positions: list[int] = []
+    for pos, char in enumerate(tokens):
+        op_precedence = get_precedence(char)
+        if op_precedence == precedence:
+            operator_positions.append(pos)
+    return operator_positions
+
+
+def get_adjacent(tokens: str, operator: str):
+    right = convert_int(tokens, operator, 1)
+    left = convert_int(tokens, operator, -1)
+    return left, right
+
 def test():
     tokens = [
-        "2*((5-1)*(2+2))", 
+        "2^3",
+        "2*((5-1)*(2+2))",
         "(5*(3-1)+2)", 
         "(2+(3-1)*5)", 
         "(2+5*1)",
-        "6/3*5"
+        "6/3*5",
+        "((6*(3/6))*3-9*(1/9))^2/(5-3)",
+        "3*(3+2)",
+        "2^(2^(2^2))",
+        "40/2*10+5.1"
         ]
-    assert len(find_all(tokens[0], "(")) == 3
-    assert find_all(tokens[0], "(") == [2, 3, 9]
+    number_answer = [
+        "8.0",
+        "32.0",
+        "12.0",
+        "12.0",
+        "7.0",
+        "10.0",
+        "32.0",
+        "15.0",
+        "65536.0",
+        "205.1"
+    ]
+    error_tokens = [
+        "3,.2",
+        "3+2+",
+        "3.2.+",
+        "3,2-.",
+        "3..2",
+        "3+(-2)",
+        "-2+3",
+        "1/(5-5)",
+        "0/0",
+        "abc*123"
+    ]
 
+    assert len(find_all(tokens[0], "(")) == 0
+    assert len(find_all(tokens[1], "(")) == 3
+    assert find_all(tokens[1], "(") == [2, 3, 9]
+    
+    # Check right answer
     for i in range(len(tokens)):
-        stack = deque()
-        print(tokens[i])
-        token_to_stack(tokens[i], stack)
+        assert eval_expr(tokens[i]) == number_answer[i]
+
+    # Display errors
+    for i in range(len(error_tokens)):
+        print(error_tokens[i])
+        print(eval_expr(error_tokens[i]))
+
 
     print("Testing Completed")
+
+if __name__ == "__main__":
+    test()
