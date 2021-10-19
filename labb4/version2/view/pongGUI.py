61089e6370e7064a0ea957f9ad2890c3745cc826
Fabian Forsman <fabian.forsman@gmail.com>
2021-10-19 11:42:56 +0200

    Krysset went crazy with programming

--- labb4/version2/view/pongGUI.py
+++ labb4/version2/view/pongGUI.py
@@ -1,15 +1,36 @@
+import pygame
 from labb4.version2.pong import Pong
-from labb4.version2.config import THEME
+from labb4.version2.config import *
 
 class PongGUI:
+    ASSET_PATH = "labb4/version2/assets/"
+    
     def __init__(self, game: Pong):
+        self.pygame.set_caption("Pong")
+        self.screen = pygame.display.set_mode(WINDOW_SIZE)
         self.game = game
+        self.game_components = [
+            self.game.board,
+            self.game.board.ball,
+            self.game.board.paddles[0],
+            self.game.board.paddles[1]
+        ]
+        self.__init_textures__()
 
     def render(self):
-        pass
+        board = self.game.board
+        for component in self.game_components:
+            self.__render_image__(component.get_img(), component.get_pos())
+
+    def __render_image__(self, image: pygame.Surface, pos: (int, int)):
+        self.screen.blit(image, pos)
 
     def __init_textures__(self):
-        pass
+        board = self.game.board
+        board.load_image(f"{self.ASSET_PATH}/{THEME}Bg.png")
+        board.ball.load_image(f"{self.ASSET_PATH}/{THEME}Ball.png")
+        board.paddles[0].load_image(f"{self.ASSET_PATH}/coolredpaddle.png")
+        board.paddles[1].load_image(f"{self.ASSET_PATH}/coolbluepaddle.png")
 
     # Called by observers when its time to render
     def notify(self):
