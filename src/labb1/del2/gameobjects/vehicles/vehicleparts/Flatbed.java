bd400d154e998f8c700b994ad5ad73236f002dd5
= <=>
2021-11-18 15:40:08 +0100

    Copied Flatbed from before

--- src/labb1/del2/gameobjects/vehicles/vehicleparts/Flatbed.java
+++ src/labb1/del2/gameobjects/vehicles/vehicleparts/Flatbed.java
@@ -1,4 +1,26 @@
 package labb1.del2.gameobjects.vehicles.vehicleparts;
 
 public class Flatbed {
+    private static final double maxFlatbedAngle = 70, minFlatbedAngle = 0;
+    private double currentFlatbedAngle;
+
+    public Flatbed() {
+        currentFlatbedAngle = 0;
+    }
+
+    public void raiseFlatbed(double angle) {
+        if (angle > 0)  {
+            currentFlatbedAngle = Math.min(angle + currentFlatbedAngle, maxFlatbedAngle);
+        }
+    }
+
+    public void lowerFlatbed(double angle) {
+        if (angle > 0) {
+            currentFlatbedAngle = Math.max(currentFlatbedAngle - angle, minFlatbedAngle);
+        }
+    }
+
+    public double getCurrentFlatbedAngle() {
+        return currentFlatbedAngle;
+    }
 }
