78b05ded43bb78f149669563a14a4fbe94e9fa8f
Krysset <maxhagman@hotmail.se>
2021-11-26 11:15:29 +0100

    Merged go, mo to vehicle

--- src/labb1/del2/vehicles/Vehicle.java
+++ src/labb1/del2/vehicles/Vehicle.java
@@ -2,18 +2,25 @@ package labb1.del2.vehicles;
 
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import labb1.del2.IMovable;
 import labb1.del2.controllers.IControllable;
-import labb1.del2.gameobjects.MovableObject;
+import labb1.del2.utils.Vector2D;
 
-public abstract class Vehicle extends MovableObject {
+public abstract class Vehicle implements IMovable {
     private static final double TURN_SPEED_CONSTANT = 100;
-
+    private static final double BASE_ROTATION = 0;
+    private Rectangle objectRect;
+    private double rotation;
     private double turnSpeed;
     private final String modelName;
     private Color color;
+    private double speed;
 
-    protected Vehicle(Rectangle rect, double turnSpeed, Color color, String modelName) {
-        super(rect);
+
+    protected Vehicle(Rectangle rect, double rotation, double turnSpeed, Color color, String modelName) {
+        this.objectRect = rect;
+        this.rotation = rotation;
+        getRect().setRotate(getRotation() * 180 / Math.PI);
         this.turnSpeed = turnSpeed;
         this.modelName = modelName;
         this.color = color;
@@ -21,7 +28,7 @@ public abstract class Vehicle extends MovableObject {
     }
 
     protected Vehicle(Rectangle rect, Color color, String modelName) {
-        this(rect, 0, color, modelName);
+        this(rect, 0,0, color, modelName);
         turnSpeed = TURN_SPEED_CONSTANT * Math.PI / getWidth();
     }
 
@@ -30,19 +37,6 @@ public abstract class Vehicle extends MovableObject {
     }
 
     public abstract String[] getHudInfo();
-
-    public static String[] concatenateStrArr(String[] arr1, String[] arr2) {
-        String[] res = new String[arr1.length + arr2.length];
-        int i = 0;
-        for (; i < arr1.length; i++) {
-            res[i] = arr1[i];
-        }
-        for (String s : arr2) {
-            res[i] = s;
-            i++;
-        }
-        return res;
-    }
 
     public final void accelerate(double amount) throws IllegalArgumentException {
         if (0 <= amount && amount <= 1) {
@@ -64,6 +58,12 @@ public abstract class Vehicle extends MovableObject {
 
     public abstract void decrementSpeed(double amount);
 
+    public final void move(double dTime) {
+        Vector2D direction = Vector2D.angleToVector2D(getRotation());
+        getRect().setX(getPosX() + dTime * speed * direction.getX());
+        getRect().setY(getPosY() + dTime * speed * direction.getY());
+    }
+
     public final void turnRight(double dTime) {
         if(getSpeed() != 0) {
             setRotation(getRotation() + turnSpeed * dTime);
@@ -75,11 +75,43 @@ public abstract class Vehicle extends MovableObject {
         }
     }
 
+
+
+    public final double getSpeed() {
+        return speed;
+    }
+    public final void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
     public final Color getColor() {
         return color;
     }
     public final String getModelName() {
         return modelName;
+    }
+    public final Rectangle getRect() {
+        return objectRect;
+    }
+    public final double getPosX() {
+        return objectRect.getX();
+    }
+    public final double getPosY() {
+        return objectRect.getY();
+    }
+    public final double getWidth() {
+        return objectRect.getWidth();
+    }
+    public final double getHeight() {
+        return objectRect.getHeight();
+    }
+    public final double getRotation() {
+        return rotation;
+    }
+    public final void setRotation(double rotation) {
+        this.rotation = rotation;
+        getRect().getTransforms().clear();
+        getRect().setRotate(rotation * 180 / Math.PI);
     }
 
     public abstract IControllable getController();
