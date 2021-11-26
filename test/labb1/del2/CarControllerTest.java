99cddd8dc6428726a2bdfc6ab39a80266fd394d0
Krysset <maxhagman@hotmail.se>
2021-11-26 11:30:52 +0100

    Added file

---
+++ test/labb1/del2/CarControllerTest.java
@@ -0,0 +1,16 @@
+package labb1.del2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarControllerTest {
+
+    @Test
+    void handleKeyPressed() {
+    }
+
+    @Test
+    void handleKeyReleased() {
+    }
+}
