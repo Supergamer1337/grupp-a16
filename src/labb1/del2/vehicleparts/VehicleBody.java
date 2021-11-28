ef786b353d8d7b06fd5ca3fe411429caecc97cb9
= <=>
2021-11-28 15:40:20 +0100

    Added getter

--- src/labb1/del2/vehicleparts/VehicleBody.java
+++ src/labb1/del2/vehicleparts/VehicleBody.java
@@ -69,6 +69,9 @@ public class VehicleBody implements IMovable {
     public final Color getColor() {
         return color;
     }
+    public double getTurnSpeed() {
+        return turnSpeed;
+    }
 
     public final double getRotation() {
         return rotation;
