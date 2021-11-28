57216b9fbe153373e6c2ba6facbb0ba7f1dec340
= <=>
2021-11-28 15:40:32 +0100

    Finished test file

---
+++ test/labb1/del2/vehicles/Volvo240Test.java
@@ -0,0 +1,25 @@
+package labb1.del2.vehicles;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Volvo240Test {
+
+    @Test
+    void getHudInfo() {
+        Volvo240 volvo = new Volvo240();
+        String[] info = volvo.getHudInfo();
+        assertEquals("Model: Volvo240", info[0]);
+        assertEquals("Engine On: false", info[1]);
+        assertEquals("Speed: 0km/h", info[2]);
+        assertEquals("Nr of doors: 4", info[3]);
+        assertEquals("Trim Factor: 1.25", info[4]);
+    }
+
+    @Test
+    void speedFactor() {
+        Volvo240 volvo = new Volvo240();
+        assertEquals(volvo.getEnginePower() * 0.01 * Volvo240.getTrimFactor(), volvo.speedFactor());
+    }
+}
