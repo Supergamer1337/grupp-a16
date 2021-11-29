efe021d88625573796a2f3b98872bc07ed92ea1a
Supergamer1337 <felix.b.aronsson@gmail.com>
2021-11-29 16:02:32 +0100

    Commented AngledFlatbed

--- src/labb1/del2/vehicleparts/AngledFlatbed.java
+++ src/labb1/del2/vehicleparts/AngledFlatbed.java
@@ -17,23 +17,47 @@ public class AngledFlatbed implements IFlatbed{
         this(DEF_MAX_Angle, DEF_MIN_ANGLE);
     }
 
+    /**
+     * Raises the flatbed.
+     */
     @Override
     public void raiseFlatbed() {
         currentFlatbedAngle = Math.min(rotationSpeed * Main.getDeltaTime() + currentFlatbedAngle, maxAngle);
     }
 
+    /**
+     * Lowers the flatbed.
+     */
     @Override
     public void lowerFlatbed() {
         currentFlatbedAngle = Math.max(currentFlatbedAngle - rotationSpeed * Main.getDeltaTime(), minAngle);
     }
 
+    /**
+     * Checks if the flatbed is raised.
+     * @return true if the flatbed is lowered, false otherwise.
+     */
     @Override
     public boolean isLowered() {
         return currentFlatbedAngle == minAngle;
     }
 
+    /**
+     * Gets the current angle of the flatbed.
+     * @return The current angle of the flatbed.
+     */
     public final double getFlatbedAngle() { return currentFlatbedAngle; }
+
+    /**
+     * Gets the maximum angle of the flatbed.
+     * @return The maximum angle of the flatbed.
+     */
     public final double getMaxAngle() { return maxAngle; }
+
+    /**
+     * Gets the minimum angle of the flatbed.
+     * @return The minimum angle of the flatbed.
+     */
     public final double getMinAngle() { return minAngle; }
 
 }
