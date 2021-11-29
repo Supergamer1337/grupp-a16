b53767c7604743e44ac1f31947635762231a1219
Fabian Forsman <fabian.forsman@gmail.com>
2021-11-29 14:58:42 +0100

    Added a bunch of tests

--- src/labb1/del2/IMovable.java
+++ src/labb1/del2/IMovable.java
@@ -1,7 +1,21 @@
 package labb1.del2;
 
 public interface IMovable {
+    /**
+     * Moves the object in the direction it is facing.
+     * @param dTime The time since the last update.
+     */
     void move(double dTime);
+
+    /**
+     * Rotates the object to the right.
+     * @param dTime The time since the last update.
+     */
     void turnRight(double dTime);
+
+    /**
+     * Rotates the object to the left.
+     * @param dTime The time since the last update.
+     */
     void turnLeft(double dTime);
 }
