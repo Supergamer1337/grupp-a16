407558f0b689e2a83a9def5e91ae94109b797b19
Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
2021-11-30 12:36:21 +0100

    Added comments to IControllable

--- src/labb1/del2/controllers/IControllable.java
+++ src/labb1/del2/controllers/IControllable.java
@@ -3,6 +3,15 @@ package labb1.del2.controllers;
 import javafx.scene.input.KeyCode;
 
 public interface IControllable {
+    /**
+     * Handles the <a href="https://docs.oracle.com/javafx/2/api/javafx/scene/input/KeyCode.html">Keycode</a> as long as it is pressed.
+     * @param key The key that is pressed.
+     */
     void handleKeyPressed(KeyCode key);
+
+    /**
+     * Handles the <a href="https://docs.oracle.com/javafx/2/api/javafx/scene/input/KeyCode.html">Keycode</a> when it is released.
+     * @param key The key that is released.
+     */
     void handleKeyReleased(KeyCode key);
 }
